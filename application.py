1716970a52d58915ae1703d9798ad2a7925f4409
Robert Schroll <rschroll@gmail.com>
2013-05-11 20:31:53 -0400

    Get multiple instances to work together
    The official ways to do this (HANDLES_COMMAND_LINE, HANDLES_OPEN) appear
    to be completely broken for Python.  So I implement my own method that
    checks whether or not it is the primary instance.  The only way I could
    get instances to talk with each other is by triggering actions.  So
    secondary instances trigger actions after processing their command lines
    to set the arguments needed when thy primary instance is activated.

--- application.py
+++ application.py
@@ -1,34 +1,24 @@
 import os
-from gi.repository import Gtk, Gio
+from gi.repository import Gtk, Gio, GObject, GLib
 from optparse import OptionParser
 from epubreader import EpubReader
-
-class NoExitParser(OptionParser):
-    
-    def __init__(self, *args, **kw):
-        OptionParser.__init__(self, *args, **kw)
-        self.exit_status = None
-    
-    def exit(self, status=0, msg=None):
-        if msg:
-            sys.stderr.write(msg)
-        self.exit_status = status
-    
-    def check_values(self, values, args):
-        return (values, args, self.exit_status)
 
 class Application(Gtk.Application):
     
     def __init__(self):
         Gtk.Application.__init__(self, application_id='apps.berg',
-                                 flags=Gio.ApplicationFlags.HANDLES_COMMAND_LINE)
-        self.connect('startup', self.startup)
-        #self.connect('activate', self.activate)
-        self.connect('command-line', self.command_line)
+                                 flags=Gio.ApplicationFlags.FLAGS_NONE)
+        self.connect('startup', self.on_startup)
+        self.connect('activate', self.on_activate)
+        self.files = None
+        self.debug = False
     
-    def startup(self, data=None):
-        for name, callback in (('open', self.on_open), ('quit', self.on_quit)):
-            action = Gio.SimpleAction(name=name)
+    def on_startup(self, data=None):
+        for name, ptype, callback in (('open', None, self.on_open),
+                                      ('quit', None, self.on_quit),
+                                      ('set-files', GLib.VariantType.new('as'), self.on_set_files),
+                                      ('set-debug', GLib.VariantType.new('b'), self.on_set_debug)):
+            action = Gio.SimpleAction(name=name, parameter_type=ptype)
             action.connect('activate', callback)
             self.add_action(action)
         
@@ -55,20 +45,13 @@ class Application(Gtk.Application):
         ''')
         self.set_app_menu(builder.get_object('app-menu'))
     
-    def command_line(self, application, command_line):
-        parser = NoExitParser(usage="Usage", description="Description", version="version")
-        parser.add_option('-d', '--debug', action='store_true', default=False,
-                          help='print debugging information')
+    def on_activate(self, application, data=None):
+        if self.files is None:
+            return
         
-        options, args, exit_status = parser.parse_args(command_line.get_arguments()[1:])
-        if exit_status != None:
-            print "Should exit with status", exit_status
-            return 0
-        
-        self.debug = options.debug
-        for arg in args:
-            if os.path.isfile(arg):
-                self.load_file(arg)
+        for f in self.files:
+            self.load_file(f)
+        self.args = None
         
         windows = self.get_windows()
         if windows:
@@ -79,8 +62,28 @@ class Application(Gtk.Application):
                 windows[0].present()
         else:
             EpubReader(self)
+    
+    def run(self, args):
+        parser = OptionParser(usage="Usage", description="Description", version="version")
+        parser.add_option('-d', '--debug', action='store_true', default=False,
+                          help='print debugging information')
+        options, args = parser.parse_args(args)
+        files = [os.path.abspath(arg) for arg in args if os.path.isfile(arg)]
         
-        return 0
+        self.register(None)
+        if self.get_is_remote():
+            self.activate_action('set-files', GLib.Variant('as', files))
+            self.activate_action('set-debug', GLib.Variant('b', options.debug))
+        else:
+            self.files = files
+            self.debug = options.debug
+        Gtk.Application.run(self, None)  # Will trigger 'activate' signal
+    
+    def on_set_files(self, action, gv_files):
+        self.files = gv_files.unpack()
+    
+    def on_set_debug(self, action, gv_debug):
+        self.debug = gv_debug.unpack()
     
     def on_open(self, action, data=None):
         dialog = Gtk.FileChooserDialog("Open...", None, Gtk.FileChooserAction.OPEN,
@@ -114,7 +117,4 @@ class Application(Gtk.Application):
 if __name__ == '__main__':
     import sys
     app = Application()
-    app.register(None)
-    if app.get_is_remote():
-        print "Already running"
-    app.run(sys.argv)
+    app.run(sys.argv[1:])
